77ae4070d3b3a878572d02a91c4d4c2756a3556d
Md. Nabil Adibur Rahman <nabil.adib.3333@gmail.com>
2025-02-21 14:55:41 +0600

    Update Student.java

--- entity/Student.java
+++ entity/Student.java
@@ -1,10 +1,14 @@
 package entity;
 
-public class Student extends User{
- 
-    public Student(String username, String password, String mail){
-        super(username, password, mail);
+public class Student extends User{    
+	public Student(String username, String password, String mail, String phone) {
+        super(null, username, password, mail, phone); 
     }
+    
+    public Student(Integer id, String username, String password, String mail, String phone) {
+        super(id, username, password, mail, phone);
+    }
+    
     public String getUsername(){
         return super.getUsername();
     }
@@ -14,8 +18,33 @@ public class Student extends User{
     public String getMail(){
         return super.getMail();
     }
-    public String toString(){
-        return super.getUsername()+"\t"+super.getPassword()+"\t"+super.getMail();
+    public String getPhone(){
+        return super.getPhone();
     }
+    public Integer getID(){
+        return super.getID();
+    }
+    public void setUsername(String username) {
+        super.setUsername(username);
+    }
+
+    public void setPassword(String password) {
+        super.setPassword(password);
+    }
+
+    public void setMail(String mail) {
+        super.setMail(mail);
+    }
+
+    public void setPhone(String phone) {
+        super.setPhone(phone);
+    }
+
+    public void setID(Integer id) {
+        super.setID(id);
+    }
+
+
+
 }
 
