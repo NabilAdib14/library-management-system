569c92dd6bf76323c39f00d1bad8cb18cf17abea
Md. Nabil Adibur Rahman <nabil.adib.3333@gmail.com>
2025-02-21 14:54:00 +0600

    Update Book.java

--- entity/Book.java
+++ entity/Book.java
@@ -1,13 +1,26 @@
 package entity;
 
 public class Book {
+    private Integer id;
     private String title;
     private String author;
     private int quantity;
-    public Book(String title, String author, int quantity){
+    private Publisher publisher;
+    public Book(Integer id, String title, String author, int quantity, Publisher publisher){
+    	this.id=id;
         this. title=title;
         this.author=author;
         this.quantity=quantity;
+        this.publisher=publisher;
+    }
+    public Book(String title, String author, int quantity, Publisher publisher) {
+        this.title = title;
+        this.author = author;
+        this.quantity = quantity;
+        this.publisher = publisher;
+    }
+    public Integer getID(){
+        return id;
     }
     public String getTitle(){
         return title;
@@ -18,4 +31,27 @@ public class Book {
     public int getQuantity(){
         return quantity;
     }
+    public Publisher getPublisher() {
+        return publisher;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
 }
